f88daac14b90f9f4f2c9213c0686165ead0503db
Matt <48924404+matteopassaro@users.noreply.github.com>
2023-02-26 19:19:15 +0100

    links

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ function github(){
     window.location.assign('https://github.com/matteopassaro',true);
     return false;
 }
+
+function sus(){
+    
+    window.location.assign('https://media.giphy.com/media/26ghbWoXv3G6ypo8o/giphy.gif',true);
+    return false;
+}
+
+function twitter(){
+    
+    window.location.assign('https://twitter.com/matteoopassaro',true);
+    return false;
+}
